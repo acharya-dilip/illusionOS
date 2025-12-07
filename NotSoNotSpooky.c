ee40ddd6009cc393f3c5937c2ea0e704731027f0
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-12-07 22:08:16 +0545

    appened the base code so that it is compatible within the IllusionOS

--- NotSoNotSpooky.c
+++ NotSoNotSpooky.c
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <curl/curl.h>
-#include <gst/gst.h>
 
 struct values {
     long long a,b;
@@ -32,10 +31,10 @@ void godsEnlightenment();
 void clearEntry();
 //Globalised Variables
 GtkWidget *entryCal;
-static void activate (GtkApplication *app,gpointer user_data) {
+void NotSoNotSpooky() {
 
     //Init of windowSpooky
-    GtkWidget *windowSpooky = gtk_application_window_new(app);
+    GtkWidget *windowSpooky = gtk_window_new();
     gtk_window_set_title(GTK_WINDOW(windowSpooky),"Calculator");
     gtk_window_present(GTK_WINDOW(windowSpooky));
 
@@ -422,15 +421,4 @@ void godsEnlightenment() {
     );
     g_object_unref(provider);
     spookyStatus=0;
-}
-
-int main(int argc, char **argv) {
-    GtkApplication *app;
-    int status;
-    app= gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
-
-    return status;
 }
