aa85a3cbcad54125476111dcbf0e1eba2754b015
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-12-07 21:42:46 +0545

    made the mainwindow of notSoSPacy as a normal window and removed the css connector

--- NotSoSpacy.c
+++ NotSoSpacy.c
@@ -66,17 +66,9 @@ GtkWidget *entryTime;
 void NotSoSpacy () {
     //Fectches the data from alarms.txt binary
     fetchData();
-    //Connecting the stylesheet
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(provider, "styles.css");
 
-    gtk_style_context_add_provider_for_display(
-        gdk_display_get_default(),
-        GTK_STYLE_PROVIDER(provider),
-        GTK_STYLE_PROVIDER_PRIORITY_USER
-    );
     //Init of windowMain
-    GtkWidget *windowMain = gtk_application_window_new(app);
+    GtkWidget *windowMain = gtk_window_new();
     gtk_window_set_default_size(GTK_WINDOW(windowMain),300,300);
     gtk_window_set_title(GTK_WINDOW(windowMain),"NotSoSpacy");
     g_signal_connect(windowMain,"destroy",G_CALLBACK(stopSound),NULL);
