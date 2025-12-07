3f62f122c64fa96fde7c0a69cd194e4f00bc6eac
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-12-07 14:59:09 +0545

    appened NotSoNotFallty code

--- NotSoNotFallty.c
+++ NotSoNotFallty.c
@@ -232,15 +232,10 @@ void createghrepo(){
     *textviewCommitMessage,
     *windowMain;
 //for a test
-static void activate(GtkApplication *app,gpointer user_data) {
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_path(provider, "styles.css");
+void NotSoNotFallty() {
+    FILE *file = fopen("filePath.txt","a");
+    fclose(file);
 
-    gtk_style_context_add_provider_for_display(
-        gdk_display_get_default(),
-        GTK_STYLE_PROVIDER(provider),
-        GTK_STYLE_PROVIDER_PRIORITY_USER
-    );
     //Executes the navigater window thingy
     initProject();
     //Widgets for MainProgram
@@ -575,16 +570,3 @@ void fetchGitCredentials() {
     gtk_editable_set_text(GTK_EDITABLE(entryToken),patoken);
 }
 
-
-int main(int argc, char **argv){
-    FILE *file = fopen("filePath.txt","a");
-    fclose(file);
-    GtkApplication *app;
-    int status;
-    app= gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
-
-    return status;
-}
